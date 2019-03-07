887fef471d7d7410a6871b7bc6b605da2a97caab
Lenore <31237758+SpaceLenore@users.noreply.github.com>
2019-03-07 16:41:27 +0100

    moved this file to dbConfig.example.js
    Delete template file & replace with example file instead. Content is the same

--- config/dbConfig.js
+++
@@ -1,9 +0,0 @@
-const connection = {
-    "base_url": "mongodb://localhost",
-    "username": "EnterDbUsernameHere",
-    "password": "EnterDbPasswordHere"
-}
-
-module.exports = {
-    connection
-}
