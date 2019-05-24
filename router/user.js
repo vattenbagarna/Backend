61f1707887d4521e9f90d55d47b7df8d7c9ad1dc
SpaceLenore <libreterra@protonmail.com>
2019-05-24 17:40:19 +0200

    added verify route

--- router/user.js
+++ router/user.js
@@ -7,6 +7,7 @@ const express               = require("express");
 const router                = new express.Router();
 const dbHandler             = require('../src/dbWrapper.js');
 const userHandler           = require('../src/userHandler.js');
+const jwtAuth               = require('../src/jwtAuthentication.js');
 
 // GET /user/all shows all users in the system.
 router.get("/all", async (req, res) => {
@@ -23,5 +24,23 @@ router.get("/all", async (req, res) => {
     }
     res.json(users);
 });
+
+//GET /user/verify verifies a token and sends back error true/false
+router.get("/verify", async (req, res) => {
+    let user = jwtAuth.verify(req.query.token);
+
+    if (user == false) {
+        res.json({
+            "error": true,
+            "info": "token failed to validate"
+        });
+    } else {
+        res.json({
+            "error": false,
+            "info": "token is valid"
+        });
+    }
+});
+
 
 module.exports = router;
